9da16a1064f06aab917d074ce7ccaf97d9c84985
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2018-02-12 05:58:02 +0500

    Translations component

---
+++ src/Comely/IO/Translations/README.md
@@ -0,0 +1,17 @@
+# Translations Component
+
+Translations component for your Comely apps
+
+## Prerequisites
+
+* PHP >= 7.1.x
+* [comelyio/comely-kernel](https://github.com/comelyio/comely-kernel) 
+* [comelyio/file-system](https://github.com/comelyio/file-system)
+
+## Installation
+
+`composer require comelyio/translations`
+
+## Resources
+
+* [Documentation](https://comely.io/translations)
---
+++ src/Comely/IO/Translations/Translations.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This file is part of Comely package.
+ * https://github.com/comelyio/comely
+ *
+ * Copyright (c) 2016-2018 Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/comelyio/comely/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace Comely\IO\Translations;
+
+use Comely\Kernel\Extend\ComponentInterface;
+
+/**
+ * Class Translations
+ * @package Comely\IO\Translations
+ */
+class Translations implements ComponentInterface
+{
+    /** @var self */
+    private static $instance;
+
+    /**
+     * Translations constructor.
+     */
+    private function __construct()
+    {
+    }
+}
