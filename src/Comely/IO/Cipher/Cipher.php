e6d0c0add0b38487d1256cd53d953c5f84c26941
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2018-02-27 17:49:02 +0500

    merge iv find within later method

--- src/Comely/IO/Cipher/Cipher.php
+++ src/Comely/IO/Cipher/Cipher.php
@@ -83,33 +83,8 @@ class Cipher implements ComponentInterface, Constants
             throw new CipherException('No default cipher key set, provide an instance of CipherKey');
         }
 
-        $this->checkKey($key); // Validate CipherKey
-
-        $iv = openssl_random_pseudo_bytes($key->_offset);
-        if (!$iv) {
-            throw new CipherException('Failed to generate Initialization Vector');
-        }
-
         $data = serialize(new Encrypted($data));
-        $encrypted = $this->openSSL->encrypt($key, $data, $iv);
-
-        return $this->encode($key, $iv . $encrypted);
-    }
-
-    /**
-     * @param CipherKey $key
-     * @param string $binary
-     * @return string
-     */
-    private function encode(CipherKey $key, string $binary): string
-    {
-        switch ($key->_encoding) {
-            case self::ENCODE_HEXITS:
-                return bin2hex($binary);
-            case self::ENCODE_BASE64:
-            default:
-                return base64_encode($binary);
-        }
+        return $this->openSSL->encrypt($key, $data, null);
     }
 
     /**
@@ -125,55 +100,12 @@ class Cipher implements ComponentInterface, Constants
             throw new CipherException('No default cipher key set, provide an instance of CipherKey');
         }
 
-        $this->checkKey($key); // Validate CipherKey
-
-        $decoded = $this->decode($key, $encrypted);
-        if (!$decoded) {
-            // Decode attempt 2, auto-detect encoding
-            if (preg_match('/^[a-fA-F0-9]+$/', $encrypted)) {
-                $decoded = bin2hex($encrypted);
-            } elseif (preg_match('/^[a-zA-Z0-9\_\+]+$/', $encrypted)) {
-                $decoded = base64_decode($encrypted);
-            }
-        }
-
-        if (!$decoded || !is_string($decoded)) {
-            throw new CipherException('Failed to decode encrypted bytes');
-        }
-
-        $decrypt = $this->openSSL->decrypt($key, $decoded);
+        $decrypt = $this->openSSL->decrypt($key, $encrypted, true, null);
         $encrypted = unserialize($decrypt);
         if (!$encrypted || !$encrypted instanceof Encrypted) {
             throw new CipherException('Failed to retrieve encrypted item');
         }
 
         return $encrypted->withdraw();
-    }
-
-    /**
-     * @param CipherKey $key
-     * @param string $encoded
-     * @return string
-     */
-    private function decode(CipherKey $key, string $encoded): string
-    {
-        switch ($key->_encoding) {
-            case self::ENCODE_HEXITS:
-                return hex2bin($encoded);
-            case self::ENCODE_BASE64:
-            default:
-                return base64_decode($encoded);
-        }
-    }
-
-    /**
-     * @param CipherKey $key
-     * @throws CipherException
-     */
-    private function checkKey(CipherKey $key): void
-    {
-        if (!$key->_cipher) {
-            throw new CipherException('No cipher method defined for provided CipherKey');
-        }
     }
 }
--- src/Comely/IO/Cipher/OpenSSL.php
+++ src/Comely/IO/Cipher/OpenSSL.php
@@ -21,7 +21,7 @@ use Comely\IO\Cipher\Keychain\CipherKey;
  * Class OpenSSL
  * @package Comely\IO\Cipher
  */
-class OpenSSL
+class OpenSSL implements Constants
 {
     /**
      * @param CipherKey $key
@@ -30,42 +30,114 @@ class OpenSSL
      * @return string
      * @throws CipherException
      */
-    public function encrypt(CipherKey $key, string $data, string $iv): string
+    public function encrypt(CipherKey $key, string $data, ?string $iv = null): string
     {
-        $encrypt = openssl_encrypt($data, $key->_cipher, $key->_key, OPENSSL_RAW_DATA, $iv);
-        if (!$encrypt) {
+        $this->checkKey($key); // Validate CipherKey
+
+        if (!$iv) {
+            $iv = openssl_random_pseudo_bytes($key->_offset);
+            if (!$iv) {
+                throw new CipherException('Failed to generate Initialization Vector');
+            }
+        }
+
+        $encrypted = openssl_encrypt($data, $key->_cipher, $key->_key, OPENSSL_RAW_DATA, $iv);
+        if (!$encrypted) {
             throw new CipherException(sprintf('Failed to encrypt using cipher "%s"', $key->_cipher));
         }
 
-        return $encrypt;
+        return $this->encode($key, $iv . $encrypted);
     }
 
     /**
      * @param CipherKey $key
-     * @param string $raw
-     * @param string $iv
+     * @param string $encrypted
+     * @param bool $decode
+     * @param null|string $iv
      * @return string
      * @throws CipherException
      */
-    public function decrypt(CipherKey $key, string $raw, ?string $iv = null): string
+    public function decrypt(CipherKey $key, string $encrypted, bool $decode = true, ?string $iv = null): string
     {
-        $subject = $raw;
+        $this->checkKey($key); // Validate CipherKey
+
+        if ($decode) {
+            $decoded = $this->decode($key, $encrypted);
+            if (!$decoded) {
+                // Decode attempt 2, auto-detect encoding
+                if (preg_match('/^[a-fA-F0-9]+$/', $encrypted)) {
+                    $decoded = bin2hex($encrypted);
+                } elseif (preg_match('/^[a-zA-Z0-9\_\+]+$/', $encrypted)) {
+                    $decoded = base64_decode($encrypted);
+                }
+            }
+
+            if (!$decoded || !is_string($decoded)) {
+                throw new CipherException('Failed to decode encrypted bytes');
+            }
+
+            $encrypted = $decoded;
+        }
+
         if (!$iv) {
-            $iv = substr($subject, 0, $key->_offset);
-            $subject = substr($subject, $key->_offset);
+            $iv = substr($encrypted, 0, $key->_offset);
+            $encrypted = substr($encrypted, $key->_offset);
         }
 
         if (!$iv) {
             throw new CipherException('Failed to retrieve Initialization Vector');
-        } elseif (!$subject) {
+        } elseif (!$encrypted) {
             throw new CipherException('Failed to retrieve encrypted bytes');
         }
 
-        $decrypt = openssl_decrypt($subject, $key->_cipher, $key->_key, OPENSSL_RAW_DATA, $iv);
+        $decrypt = openssl_decrypt($encrypted, $key->_cipher, $key->_key, OPENSSL_RAW_DATA, $iv);
         if (!$decrypt) {
             throw new CipherException('Failed to decrypt data');
         }
 
         return $decrypt;
+    }
+
+    /**
+     * @param CipherKey $key
+     * @throws CipherException
+     */
+    private function checkKey(CipherKey $key): void
+    {
+        if (!$key->_cipher) {
+            throw new CipherException('No cipher method defined for provided CipherKey');
+        }
+    }
+
+    /**
+     * @param CipherKey $key
+     * @param string $binary
+     * @return string
+     */
+    private function encode(CipherKey $key, string $binary): string
+    {
+        switch ($key->_encoding) {
+            case self::ENCODE_HEXITS:
+                return bin2hex($binary);
+            case self::ENCODE_BASE64:
+            default:
+                return base64_encode($binary);
+        }
+    }
+
+    /**
+     * @param CipherKey $key
+     * @param string $encoded
+     * @return string
+     */
+    private function decode(CipherKey $key, string $encoded): string
+    {
+        switch ($key->_encoding) {
+            case self::ENCODE_HEXITS:
+                return hex2bin($encoded);
+            case self::ENCODE_BASE64:
+            default:
+                return base64_decode($encoded);
+        }
     }
 }
